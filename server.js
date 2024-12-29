48042573ef7563613924049ded3e4d89dcaeb97d
user_Gyrus <nandakumarblj@gmail.com>
2024-12-29 12:58:39 +0530

    Bug Fixes 2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const http = require("http");
-const dotnev = require("dotenv");
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const userRouter = require("./routes/userRoute");
 const socketio = require("socket.io");
@@ -8,45 +8,47 @@ const cors = require("cors");
 const socketIo = require("./socket");
 const groupRouter = require("./routes/groupRoute");
 const messageRouter = require("./routes/messageRoute");
-dotnev.config();
+
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 
 const io = socketio(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173"], // Adjust this for your frontend URL
     methods: ["GET", "POST"],
     credentials: true,
   },
 });
 
-//middlewares
+// Middlewares
 app.use(cors());
 app.use(express.json());
-//connect to the database
+
+// Connect to the database
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTsopology: true,
+    useUnifiedTopology: true, // Corrected option
     serverSelectionTimeoutMS: 15000,
     connectTimeoutMS: 30000,
   })
   .then(() => {
-    console.log(`Connection Establised with the Database`);
+    console.log(`Connection established with the database`);
   })
   .catch((err) => {
     console.log(`Connection with database failed`, err);
   });
 
-//initialise the socket
+// Initialize the socket
 socketIo(io);
 
 app.use("/api/users", userRouter);
 app.use("/api/groups", groupRouter);
 app.use("/api/messages", messageRouter);
 
-//start the server
+// Start the server
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
